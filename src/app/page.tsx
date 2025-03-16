6b419bbd4ecce6da6d96276bdd93f007ee6c7eb7
dosepehr <dosepehr@gmail.com>
2025-03-16 16:46:10 +0330

    edit tailwind container class

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
     return (
         <div className=''>
-            <div className=' bg-base-25  text-primary focus:bg-primary-focus'>
+            <div className=' bg-base-25  text-primary focus:bg-primary-focus container'>
                 This div will always use light theme
                 <span>This span will always use retro theme!</span>
             </div>
