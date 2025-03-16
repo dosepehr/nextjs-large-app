c537f7daf79269ccaa04dfae071b128323efc33e
dosepehr <dosepehr@gmail.com>
2025-03-16 17:20:08 +0330

    edit tailwind config

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,7 +1,19 @@
+import Image from 'next/image';
 import React, { FC } from 'react';
 
 const Header: FC = () => {
-    return <header className='border-b dark:border-base-content'>header</header>;
+    return (
+        <header className='border-b dark:border-base-content'>
+            <div className='container flex items-center justify-between lg:bg-red-500'>
+                <Image
+                    src={'/icons/logo.svg'}
+                    width={100}
+                    height={36}
+                    alt='classbon'
+                />
+            </div>
+        </header>
+    );
 };
 
 export default Header;
