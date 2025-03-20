c5dc9c8cfb3763a020d6c49a0fda06fd61531490
dosepehr <dosepehr@gmail.com>
2025-03-20 18:53:19 +0330

    add verify page

---
+++ src/app/(auth)/verify/_types/verify.type.ts
@@ -0,0 +1,4 @@
+export interface Verify {
+    mobile: string;
+    code: string;
+}
--- src/app/(auth)/verify/page.tsx
+++ src/app/(auth)/verify/page.tsx
@@ -1,13 +1,69 @@
-import Link from 'next/link';
+'use client';
+import Button from '@/components/ui/button';
+import Input from '@/components/ui/input';
+import { useCustomMutation } from '@/utils/api/hooks/useCustomMutation';
 import React from 'react';
+import { useForm } from 'react-hook-form';
+import { Verify } from './_types/verify.type';
+import { useRouter } from 'next/navigation';
 
-const page = () => {
+const Page = () => {
+    const router = useRouter();
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+        getValues,
+    } = useForm<Verify>();
+
+    const { mutate, isPending } = useCustomMutation<Verify>({
+        mutationKey: ['Verify'],
+        url: '/verify',
+        method: 'POST',
+        onSuccess() {
+            router.push(`/Verifyrify?mobile=${getValues('mobile')}`);
+        },
+    });
+
+    const onSubmit = (data: Verify) => {
+        console.log(data);
+        // mutate(data);
+    };
+
     return (
         <div>
-            verify
-            <Link href={'/signin'}>signin</Link>
+            <h5 className='text-2xl'>ورود | ثبت نام</h5>
+            <p className='mt-2'>
+                دنیای شگفت انگیز برنامه نویسی در انتظار شماست!
+            </p>
+            <form
+                className='flex flex-col gap-6 mt-16'
+                onSubmit={handleSubmit(onSubmit)}
+            >
+                <Input<Verify>
+                    {...register('code', {
+                        required: 'کد تایید الزامی است',
+                        maxLength: {
+                            value: 5,
+                            message: 'کد تایید باید 5 رقم باشد',
+                        },
+                        minLength: {
+                            value: 5,
+                            message: 'کد تایید باید 5 رقم باشد',
+                        },
+                    })}
+                    theme='primary'
+                    placeholder='کد تایید'
+                    errors={errors}
+                    name='code'
+                    type='number'
+                />
+                <Button type='submit' theme='primary' isLoading={isPending}>
+                    تایید و دریافت کد
+                </Button>
+            </form>
         </div>
     );
 };
 
-export default page;
+export default Page;
