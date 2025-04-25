7e57a19389f0bf8abfe8444b5c7aa5d3b91c5df0
dosepehr <dosepehr@gmail.com>
2025-04-25 21:52:58 +0330

    add storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,21 +1,17 @@
-import type { StorybookConfig } from "@storybook/experimental-nextjs-vite";
+import type { StorybookConfig } from '@storybook/experimental-nextjs-vite';
 
 const config: StorybookConfig = {
-  "stories": [
-    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-essentials",
-    "@storybook/addon-onboarding",
-    "@chromatic-com/storybook",
-    "@storybook/experimental-addon-test"
-  ],
-  "framework": {
-    "name": "@storybook/experimental-nextjs-vite",
-    "options": {}
-  },
-  "staticDirs": [
-    "..\\public"
-  ]
+    stories: ['../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+    addons: [
+        '@storybook/addon-essentials',
+        '@storybook/addon-onboarding',
+        '@chromatic-com/storybook',
+        '@storybook/experimental-addon-test',
+    ],
+    framework: {
+        name: '@storybook/experimental-nextjs-vite',
+        options: {},
+    },
+    staticDirs: ['../public'],
 };
 export default config;
